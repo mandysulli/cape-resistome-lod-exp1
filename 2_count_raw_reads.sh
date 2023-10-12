7fc9a134126d9d83357832873f2d874c79a21987
mandysulli <89869003+mandysulli@users.noreply.github.com>
2023-10-12 18:16:01 -0400

    Create 2_count_raw_reads.sh

---
+++ 2_count_raw_reads.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=count
+#SBATCH --partition=glenn_p
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=20gb
+#SBATCH --time=8:00:00
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mandyh@uga.edu
+
+#set directory
+cd /scratch/mandyh/cape-resistome-lod-exp1/Raw_data
+
+for i in {1..37}
+do
+echo "CAPE_$i"
+echo $(zcat CAPE_$i\.1.fq.gz |wc -l)/4|bc
+echo $(zcat CAPE_$i\.2.fq.gz |wc -l)/4|bc
+done
